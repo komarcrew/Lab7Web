d5882d361023351fa9598d0be2de485e8ee3fc0a
Komarudin <101499377+komarcrew@users.noreply.github.com>
2022-05-17 21:46:04 +0700

    Add files via upload

---
+++ PVariable.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>PHP Dasar</title>
+</head>
+<body>
+	<h1>Predefine Variable</h1>
+	<?php 
+	echo 'Selamat Datang ' . $_GET['nama']; ?>
+</body>
+</html>
+
---
+++ php_dasar.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>PHP Dasar</title>
+</head>
+<body>
+	<h1>Belajar PHP Dasar</h1>
+	<?php echo "Hello World";?>
+	<h2>Belajar Menggunakan Variabel</h2>
+	<?php
+	$nim = "312010068";
+	$nama = 'Komarudin';
+	echo "NIM : " . $nim . "<br>";
+	echo "Nama : $nama"; ?>
+</body>
+</html>
